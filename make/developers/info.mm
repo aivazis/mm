6578822ec75cf5a59f2fde94d237b01dbaa88bcc
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-05-21 10:15:07 -0700

    make/developers: repaired and simplified the {developer} info screen

--- make/developers/info.mm
+++ make/developers/info.mm
@@ -10,26 +10,18 @@
 
 # developer info
 developer.info:
-	${call log.sec,"developer", "developer info"}
-	${call log.var,compilers,$(developer.compilers)}
-	${call log.sec,"  c options",}
-	${call log.var,flags,$(developer.c.flags)}
-	${call log.var,defines,$(developer.c.defines)}
-	${call log.var,incpath,$(developer.c.incpath)}
-	${call log.var,ldflags,$(developer.c.ldflags)}
-	${call log.var,libraries,$(developer.c.libraries)}
-	${call log.sec,"  c++ options",}
-	${call log.var,flags,$(developer.c++.flags)}
-	${call log.var,defines,$(developer.c++.defines)}
-	${call log.var,incpath,$(developer.c++.incpath)}
-	${call log.var,ldflags,$(developer.c++.ldflags)}
-	${call log.var,libraries,$(developer.c++.libraries)}
-	${call log.sec,"  fortran options",}
-	${call log.var,flags,$(developer.fortran.flags)}
-	${call log.var,defines,$(developer.fortran.defines)}
-	${call log.var,incpath,$(developer.fortran.incpath)}
-	${call log.var,ldflags,$(developer.fortran.ldflags)}
-	${call log.var,libraries,$(developer.fortran.libraries)}
+	${call log.sec,"developer", "user specific options and overrides"}
+	${call log.var,compiler choices,$(developer.$(developer).compilers)}
+	${foreach \
+            language, \
+            $(languages),\
+            ${call log.sec, "  $(language) options",}; \
+            ${foreach \
+                category, \
+                $(languages.$(language).options.compile) $(languages.$(language).options.link), \
+                ${call log.var,$(category), $(developers.$(developer).$(language).$(category))}; \
+            } \
+        }
 
 # show me
 # ${info -- done with developers.info}
