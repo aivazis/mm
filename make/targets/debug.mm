394d49913aa98317eccb86f0d04af36203183949
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-06-11 20:49:55 +0300

    make/targets: {debug} now adds the {DEBUG} macro to its {defines} pile of every compiled language

--- make/targets/debug.mm
+++ make/targets/debug.mm
@@ -12,7 +12,11 @@ targets.debug.description := compiling with support for debugging
 ${eval ${call target.init,debug}}
 
 # adjust
-${call target.adjust,debug,c c++ fortran,flags ldflags}
+${call target.adjust,debug,c c++ cuda fortran,flags ldflags}
+# define the DEBUG macro
+${foreach language,c c++ cuda cython fortran, \
+    ${eval targets.debug.$(language).defines += DEBUG} \
+}
 
 # build my info target
 ${eval ${call target.info.flags,debug}}
