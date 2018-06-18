cae88695a8bf95720480129744af6d863962f1fe
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-06-18 16:34:48 +0300

    make/compilers: added naïve support for {cython}

--- make/compilers/cython.mm
+++ make/compilers/cython.mm
@@ -7,4 +7,31 @@
 
 compiler.cython := cython
 
+# prefices for specific categories
+cython.prefix.flags :=
+cython.prefix.defines := -D
+cython.prefix.incpath := -I
+
+cython.prefix.ldflags :=
+cython.prefix.libpath := -L
+cython.prefix.libraries := -l
+
+# compile time flags
+cython.compile.base :=
+cython.compile.only := -c
+cython.compile.output := -o
+cython.compile.generate-dependencies :=
+
+# symbols and optimization
+cython.debug := -g
+cython.opt := -O3
+cython.cov := --coverage
+cython.prof := -pg
+cython.shared :=
+
+# link time flags
+cython.link.output := -o
+# link a dynamically loadable library
+cython.link.dll :=
+
 # end of file
