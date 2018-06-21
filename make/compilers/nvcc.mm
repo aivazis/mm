2c9c8fee917d79d2b973cb74d39ca12c7a1bba58
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-06-21 10:06:53 -0700

    make/compilers: added {makedep} support for {nvcc}

--- make/compilers/nvcc.mm
+++ make/compilers/nvcc.mm
@@ -17,9 +17,10 @@ nvcc.prefix.libpath := -L
 nvcc.prefix.libraries := -l
 
 # compile time flags
-nvcc.compile.base :=
 nvcc.compile.only := -dc
 nvcc.compile.output := -o
+nvcc.compile.makedep := -M
+nvcc.compile.base :=
 
 # symbols and optimization
 nvcc.debug := -g
@@ -29,15 +30,39 @@ nvcc.prof := -pg
 nvcc.shared := -Xcompiler -fPIC
 
 # language level
-nvcc.std.c++98 := -std=c++98
+nvcc.std.c++03 := -std=c++03
 nvcc.std.c++11 := -std=c++11
 nvcc.std.c++14 := -std=c++14
-nvcc.std.c++17 := -std=c++17
 
 # link time flags
 nvcc.link.output := -o
 nvcc.link.shared :=
 # link a dynamically loadable library
 nvcc.link.dll := -shared
+
+# dependency generation
+# nvcc does this in two passes: the dependency file gets generated during an additional compilation
+# phase so there is an extra step necessary to build it
+# N.B. the adjustments necessary to transform the dependency file into canonical form appear to be
+# the same as {gcc}; this may be the case if nvcc uses a different compiler underneath, so this is
+# something that has to be investigated
+#   usage: nvcc.makedep {source} {depfile} {external dependencies}
+define nvcc.makedep =
+    nvcc \
+        $(1) \
+        $(nvcc.compile.base) \
+        $(nvcc.compile.makedep) \
+        ${call compiler.compile.options,cuda,nvcc,$(3)} > $(2) ; \
+    $(cp) $(2) $(2).tmp ; \
+    $(sed) \
+        -e 's/\#.*//' \
+        -e 's/^[^:]*: *//' \
+        -e 's/ *\\$$$$//' \
+        -e '/^$$$$/d' \
+        -e 's/$$$$/ :/' \
+        $(2) >> $(2).tmp ; \
+    $(mv) $(2).tmp $(2)
+endef
+
 
 # end of file
