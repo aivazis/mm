69bde2c8b00c13b27a3acba2a8ca97b7512ba241
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-07-16 16:59:16 +0300

    make/languages: {cython} got a makeover: repaired the list of extensions, removed the linking flags, and gave it a special compile command; the compile command is almost certainly not sufficient but avoids the problems the fully general one has in this case, such as expanding the target defines, etc.

--- make/languages/cython.mm
+++ make/languages/cython.mm
@@ -6,8 +6,8 @@
 #
 
 # cython
-languages.cython.sources := .pxd
-languages.cython.headers :=
+languages.cython.sources := .pyx
+languages.cython.headers := .pxd .pxi
 
 # language predicates
 languages.cython.compiled := yes
@@ -15,7 +15,15 @@ languages.cython.interpreted :=
 
 # flags
 languages.cython.categories.compile := flags defines incpath
-languages.cython.categories.link := ldflags libpath libraries
+languages.cython.categories.link :=
+
+# build a compile command line
+#  usage: languages.cython.compile {source-file} {target-object} {dependencies}
+define languages.cython.compile =
+${strip
+    cython $(1) $(cython.compile.output) $(2)
+}
+endef
 
 
 # end of file
