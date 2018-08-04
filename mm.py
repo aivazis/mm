9ba76c6c6b22ae4d5773e37fb1653a1739e40cae
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-08-04 23:17:21 +0300

    mm: changed the character used to join the target variants together from a {,} to a {-}; {nvcc} seemed unable to pass paths with {,} to the compiler and linker correctly

--- mm.py
+++ mm.py
@@ -223,7 +223,7 @@ class mm(pyre.application, family='pyre.applications.mm', namespace='mm'):
         # compute the target
         target = host.platform + "-" + host.cpus.architecture
         # assemble the variants
-        variants = ",".join(self.target)
+        variants = "-".join(self.target)
         # compute the current target tag
         tag = "-".join(filter(None, (variants, target)))
 
