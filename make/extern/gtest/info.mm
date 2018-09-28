73526d1e85472f0c4f332cff80e7862d062a53dc
Michael Aïvázis <michael.aivazis@para-sim.com>
2018-09-28 10:52:40 -0700

    make/extern: added support for {gtest}

---
+++ make/extern/gtest/info.mm
@@ -0,0 +1,30 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# show me
+# ${info -- gtest.info}
+
+# display the gtest configuration
+extern.gtest.info:
+	${call log.sec,"gtest",}
+	${call log.var,"version",$(gtest.version)}
+	${call log.var,"configuration file",$(gtest.config)}
+	${call log.var,"home",$(gtest.dir)}
+	${call log.var,"compiler flags",$(gtest.flags)}
+	${call log.var,"defines",$(gtest.defines)}
+	${call log.var,"incpath",$(gtest.incpath)}
+	${call log.var,"linker flags",$(gtest.ldflags)}
+	${call log.var,"libpath",$(gtest.libpath)}
+	${call log.var,"libraries",$(gtest.libraries)}
+	${call log.var,"dependencies",$(gtest.dependencies)}
+	${call log.var,"c++ compile line",${call extern.compile.options,c++,gtest}}
+	${call log.var,"c++ link line",${call extern.link.options,c++,gtest}}
+
+# show me
+# ${info -- done with gtest.info}
+
+# end of file
---
+++ make/extern/gtest/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# show me
+# ${info -- gtest.init}
+
+# add me to the pile
+extern += ${if ${findstring gtest,$(extern)},,gtest}
+
+# compiler flags
+gtest.flags ?=
+# enable {gtest} aware code
+gtest.defines := WITH_GTEST
+# the canonical form of the include directory
+gtest.incpath ?= $(gtest.dir)/include
+
+# linker flags
+gtest.ldflags ?=
+# the canonical form of the lib directory
+gtest.libpath ?= $(gtest.dir)/lib
+# the names of the libraries
+gtest.libraries += gtest
+
+# my dependencies
+gtest.dependencies =
+
+# show me
+# ${info -- done with gtest.init}
+
+# end of file
