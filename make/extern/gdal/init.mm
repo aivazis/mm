72d6a1fe61fa9f87ddbe7bfde8d0fcb9b9a5a955
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-07-13 17:44:43 +0300

    make/extern: added support for {gdal}

---
+++ make/extern/gdal/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2018 all rights reserved
+#
+
+# show me
+# ${info -- gdal.init}
+
+# add me to the pile
+extern += ${if ${findstring gdal,$(extern)},,gdal}
+
+# compiler flags
+gdal.flags ?=
+# enable {gdal} aware code
+gdal.defines := WITH_GDAL
+# the canonical form of the include directory
+gdal.incpath ?= $(gdal.dir)/include
+
+# linker flags
+gdal.ldflags ?=
+# the canonical form of the lib directory
+gdal.libpath ?= $(gdal.dir)/lib
+# the names of the libraries
+gdal.libraries := gdal
+
+# my dependencies
+gdal.dependencies =
+
+# show me
+# ${info -- done with gdal.init}
+
+# end of file
