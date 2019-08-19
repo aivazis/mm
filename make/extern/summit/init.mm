d1b2061529a07ec5e1212da8b3852e2d93f3084c
Michael Aïvázis <michael.aivazis@para-sim.com>
2019-08-19 12:50:59 +0300

    make/extern: added {summit}

---
+++ make/extern/summit/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2019 all rights reserved
+#
+
+# show me
+# ${info -- summit.init}
+
+# add me to the pile
+extern += ${if ${findstring summit,$(extern)},,summit}
+
+# compiler flags
+summit.flags ?=
+# enable {summit} aware code
+summit.defines :=
+# the canonical form of the include directory
+summit.incpath ?= $(summit.dir)/include
+
+# linker flags
+summit.ldflags ?=
+# the canonical form of the lib directory
+summit.libpath ?= $(summit.dir)/lib
+# the names of the libraries
+summit.libraries := summit
+
+# my dependencies
+summit.dependencies = gsl slepc petsc metis parmetis hdf5 mpi vtk fortran
+
+# show me
+# ${info -- done with summit.init}
+
+# end of file
