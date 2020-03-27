925443e009a2f6455518b04532b0fb26854155eb
Bianca Giovanardi <bianca.giovanardi@polimi.it>
2020-03-27 18:36:38 -0400

    make/external: added support for {cgal}

---
+++ make/extern/cgal/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2019 all rights reserved
+#
+
+# show me
+# ${info -- cgal.init}
+
+# add me to the pile
+extern += ${if ${findstring cgal,$(extern)},,cgal}
+
+# compiler flags
+cgal.flags ?=
+# enable {cgal} aware code
+cgal.defines := WITH_CGAL
+# the canonical form of the include directory
+cgal.incpath ?= $(cgal.dir)/include
+
+# linker flags
+cgal.ldflags ?=
+# the canonical form of the lib directory
+cgal.libpath ?= $(cgal.dir)/lib
+# the name of the library
+cgal.libraries := CGAL
+
+# initialize the list of my dependencies
+cgal.dependencies =
+
+# show me
+# ${info -- done with cgal.init}
+
+# end of file
