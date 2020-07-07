89fd2e0da99bb19f8e75df0dba1ee68438493cbf
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-07-07 11:46:46 +0300

    make/extern: added support for {p2}, the {pyre 2.x} transitional package

---
+++ make/extern/p2/info.mm
@@ -0,0 +1,30 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+# show me
+# ${info -- p2.info}
+
+# display the p2 configuration
+extern.p2.info:
+	@${call log.sec,"p2",}
+	@${call log.var,"version",$(p2.version)}
+	@${call log.var,"configuration file",$(p2.config)}
+	@${call log.var,"home",$(p2.dir)}
+	@${call log.var,"compiler flags",$(p2.flags)}
+	@${call log.var,"defines",$(p2.defines)}
+	@${call log.var,"incpath",$(p2.incpath)}
+	@${call log.var,"linker flags",$(p2.ldflags)}
+	@${call log.var,"libpath",$(p2.libpath)}
+	@${call log.var,"libraries",$(p2.libraries)}
+	@${call log.var,"dependencies",$(p2.dependencies)}
+	@${call log.var,"c++ compile line",${call extern.compile.options,c++,p2}}
+	@${call log.var,"c++ link line",${call extern.link.options,c++,p2}}
+
+# show me
+# ${info -- done with p2.info}
+
+# end of file
---
+++ make/extern/p2/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+# show me
+# ${info -- p2.init}
+
+# add me to the pile
+extern += ${if ${findstring p2,$(extern)},,p2}
+
+# compiler flags
+p2.flags ?=
+# enable {p2} aware code
+p2.defines += WITH_PYRE WITH_JOURNAL
+# the canonical form of the include directory
+p2.incpath ?= $(p2.dir)/include
+
+# linker flags
+p2.ldflags ?=
+# the canonical form of the lib directory
+p2.libpath ?= $(p2.dir)/lib
+# the names of the libraries
+p2.libraries ?= p2 j2
+
+# my dependencies
+p2.dependencies =
+
+# show me
+# ${info -- done with p2.init}
+
+# end of file
