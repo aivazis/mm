d221e80b9814abeb4896a31acd7aa87f1fa4c877
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-11-19 08:51:52 -0800

    make/extern: added support for {eigen}

---
+++ make/extern/eigen/info.mm
@@ -0,0 +1,30 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+# show me
+# ${info -- eigen.info}
+
+# display the eigen configuration
+extern.eigen.info:
+	@${call log.sec,"eigen",}
+	@${call log.var,"version",$(eigen.version)}
+	@${call log.var,"configuration file",$(eigen.config)}
+	@${call log.var,"home",$(eigen.dir)}
+	@${call log.var,"compiler flags",$(eigen.flags)}
+	@${call log.var,"defines",$(eigen.defines)}
+	@${call log.var,"incpath",$(eigen.incpath)}
+	@${call log.var,"linker flags",$(eigen.ldflags)}
+	@${call log.var,"libpath",$(eigen.libpath)}
+	@${call log.var,"libraries",$(eigen.libraries)}
+	@${call log.var,"dependencies",$(eigen.dependencies)}
+	@${call log.var,"c++ compile line",${call extern.compile.options,c++,eigen}}
+	@${call log.var,"c++ link line",${call extern.link.options,c++,eigen}}
+
+# show me
+# ${info -- done with eigen.info}
+
+# end of file
---
+++ make/extern/eigen/init.mm
@@ -0,0 +1,34 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+# show me
+# ${info -- eigen.init}
+
+# add me to the pile
+extern += ${if ${findstring eigen,$(extern)},,eigen}
+
+# compiler flags
+eigen.flags ?=
+# enable {eigen} aware code
+eigen.defines := WITH_EIGEN3
+# the canonical form of the include directory
+eigen.incpath ?= $(eigen.dir)/include/eigen3
+
+# linker flags
+eigen.ldflags ?=
+# the canonical form of the lib directory
+eigen.libpath ?=
+# the names of the libraries
+eigen.libraries :=
+
+# my dependencies
+eigen.dependencies =
+
+# show me
+# ${info -- done with eigen.init}
+
+# end of file
