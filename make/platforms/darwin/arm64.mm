e655332a77374c229a12b66cf1f53c55b018826d
Michael Aïvázis <michael.aivazis@para-sim.com>
2020-12-30 08:51:28 +0200

    make/platforms: primed support for the new apple silicon; it identifies itself as {arm64}

---
+++ make/platforms/darwin/arm64.mm
@@ -0,0 +1,16 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2020 all rights reserved
+#
+
+# default compilers; specify as many as necessary in the form {family} or {family/language}
+platform.compilers = gcc python cython
+
+# clean up the debug symbols compilers leave behind
+#     platform.clean {stem}
+platform.clean = $(1).dSYM
+
+
+# end of file
