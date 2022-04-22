9d96df86d4b71d40b08830c056adbbe654974656
Raul Radovitzky <rapa@mit.edu>
2022-04-22 14:56:00 -0400

    added extern support for yaml c++ libraries

---
+++ make/extern/yaml/init.mm
@@ -0,0 +1,39 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2022 all rights reserved
+#
+
+# show me
+# ${info -- yaml.init}
+
+# add me to the pile
+extern += ${if ${findstring yaml,$(extern)},,yaml}
+
+# find my configuration file
+yaml.config := ${dir ${call extern.config,yaml}}
+
+# the flavor
+yaml.flavor := -cpp
+# compiler flags
+yaml.flags ?=
+# enable {yaml} aware code
+yaml.defines := WITH_YAML
+# the canonical form of the include directory
+yaml.incpath ?= $(yaml.dir)/include
+
+# linker flags
+yaml.ldflags ?=
+# the canonical form of the lib directory
+yaml.libpath ?= $(yaml.dir)/lib
+# the name of the library
+yaml.libraries := ${addprefix yaml,$(yaml.flavor)}
+
+# initialize the list of my dependencies
+yaml.dependencies =
+
+# show me
+# ${info -- done with yaml.init}
+
+# end of file
