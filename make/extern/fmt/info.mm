c14bdbe2a0bd2d8392c5f183dd605c9d2e5a6456
Theo Rulko <trulko@mit.edu>
2023-01-16 10:57:44 -0500

    make/extern: added support for {cantera}

---
+++ make/extern/fmt/info.mm
@@ -0,0 +1,30 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# parasim
+# (c) 1998-2023 all rights reserved
+#
+
+# show me
+# ${info -- fmt.info}
+
+# display the summit configuration
+extern.fmt.info:
+	@${call log.sec,"fmt",}
+	@${call log.var,"version",$(fmt.version)}
+	@${call log.var,"configuration file",$(fmt.config)}
+	@${call log.var,"home",$(fmt.dir)}
+	@${call log.var,"compiler flags",$(fmt.flags)}
+	@${call log.var,"defines",$(fmt.defines)}
+	@${call log.var,"incpath",$(fmt.incpath)}
+	@${call log.var,"linker flags",$(fmt.ldflags)}
+	@${call log.var,"libpath",$(fmt.libpath)}
+	@${call log.var,"libraries",$(fmt.libraries)}
+	@${call log.var,"dependencies",$(fmt.dependencies)}
+	@${call log.var,"c++ compile line",${call extern.compile.options,c++,fmt}}
+	@${call log.var,"c++ link line",${call extern.link.options,c++,fmt}}
+
+# show me
+# ${info -- done with fmt.info}
+
+# end of file
