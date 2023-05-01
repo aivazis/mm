1bb4157dd04e753765019c2de7b0d5197567fe9f
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-01 07:56:02 +0300

    mm: moved to {pyre-1.12.0} for bootstrapping

--- mm.py
+++ mm.py
@@ -22,7 +22,7 @@ except ImportError:
     os.makedirs(pdir, exist_ok=True)
 
     # the target version
-    release = "v1.11.2"
+    release = "v1.12.0"
     # the bootstrapping  package
     boot = "pyre-boot.zip"
 
