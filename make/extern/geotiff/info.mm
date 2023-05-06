cae71fb84e691cd9762810c386a417ddb2137ef9
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-05-06 09:38:18 +0300

    make/extern: added support for {geotiff}

---
+++ make/extern/geotiff/info.mm
@@ -0,0 +1,28 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2023 all rights reserved
+
+# show me
+# ${info -- geotiff.info}
+
+# display the geotiff configuration
+extern.geotiff.info:
+	@${call log.sec,"geotiff",}
+	@${call log.var,"version",$(geotiff.version)}
+	@${call log.var,"configuration file",$(geotiff.config)}
+	@${call log.var,"home",$(geotiff.dir)}
+	@${call log.var,"compiler flags",$(geotiff.flags)}
+	@${call log.var,"defines",$(geotiff.defines)}
+	@${call log.var,"incpath",$(geotiff.incpath)}
+	@${call log.var,"linker flags",$(geotiff.ldflags)}
+	@${call log.var,"libpath",$(geotiff.libpath)}
+	@${call log.var,"libraries",$(geotiff.libraries)}
+	@${call log.var,"dependencies",$(geotiff.dependencies)}
+	@${call log.var,"c++ compile line",${call extern.compile.options,c++,geotiff}}
+	@${call log.var,"c++ link line",${call extern.link.options,c++,geotiff}}
+
+# show me
+# ${info -- done with geotiff.info}
+
+# end of file
---
+++ make/extern/geotiff/init.mm
@@ -0,0 +1,35 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2023 all rights reserved
+
+# show me
+# ${info -- geotiff.init}
+
+# add me to the pile
+extern += ${if ${findstring geotiff,$(extern)},,geotiff}
+
+# # find my configuration file
+geotiff.config := ${dir ${call extern.config,geotiff}}
+
+# compiler flags
+geotiff.flags ?=
+# enable {geotiff} aware code
+geotiff.defines := WITH_GEOTIFF
+# the canonical form of the include directory
+geotiff.incpath ?= $(geotiff.dir)/include
+
+# linker flags
+geotiff.ldflags ?=
+# the canonical form of the lib directory
+geotiff.libpath ?= $(geotiff.dir)/lib
+# the names of the libraries
+geotiff.libraries := geotiff
+
+# my dependencies
+geotiff.dependencies =
+
+# show me
+# ${info -- done with geotiff.init}
+
+# end of file
