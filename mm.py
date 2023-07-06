6bc6dd0dc9b60e440924d850b5a4c1040c50d0da
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-07-06 09:56:12 +0300

    mm: the bootstrap logic now pulls {pyre-1.12.1}

--- mm.py
+++ mm.py
@@ -22,7 +22,7 @@ except ImportError:
     os.makedirs(pdir, exist_ok=True)
 
     # the target version
-    release = "v1.12.0"
+    release = "v1.12.1"
     # the bootstrapping  package
     boot = "pyre-boot.zip"
 
