26a1eb8d035c68ce4598ba993e06c3589ca44701
Michael Aïvázis <michael.aivazis@para-sim.com>
2026-06-06 09:50:50 +0300

    make/extern: rename yaml -> yaml-cpp; it is the C++ library that was squatting on the canonical C libyaml name (also fixes the macports/dpkg recipe keys and cantera dependency), freeing yaml for a future libyaml external

---
+++ make/extern/yaml-cpp/init.mm
@@ -0,0 +1,33 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis <michael.aivazis@para-sim.com>
+# (c) 1998-2026 all rights reserved
+
+
+# add me to the pile
+extern += ${if ${findstring yaml-cpp,$(extern)},,yaml-cpp}
+
+# find my configuration file
+yaml-cpp.config := ${dir ${call extern.config,yaml-cpp}}
+
+# compiler flags
+yaml-cpp.flags ?=
+# enable {yaml-cpp} aware code
+yaml-cpp.defines ?= WITH_YAML_CPP
+# the canonical form of the include directory
+yaml-cpp.incpath ?= $(yaml-cpp.dir)/include
+
+# linker flags
+yaml-cpp.ldflags ?=
+# the canonical form of the lib directory
+yaml-cpp.libpath ?= $(yaml-cpp.dir)/lib
+# its rpath
+yaml-cpp.rpath = $(yaml-cpp.libpath)
+# the name of the library
+yaml-cpp.libraries := yaml-cpp
+
+# initialize the list of my dependencies
+yaml-cpp.dependencies =
+
+
+# end of file
